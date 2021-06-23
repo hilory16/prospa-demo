c77a6c0c1e38bd9817452b4c7e1743ea1afdcfe1
Emmanuel Ilori <hiloryolamide@gmail.com>
2021-06-23 13:15:55 +0100

    Helmet each page

--- src/components/OnboardingFormArea/index.js
+++ src/components/OnboardingFormArea/index.js
@@ -6,9 +6,9 @@ const Index = ({children, active,signupLink, handlePage}) => {
     return (
         <Wrapper className="onboarding-form-area px-2 ">
             <div className="container">
-                <div className={`d-flex ${active ==="biodata" ? 'justify-content-end':'justify-content-between'} sign-in top-bar align-items-center flex-wrap flex-md-nowrap`}>
+                <div className={`d-flex ${active !=="business" ? 'justify-content-end':'justify-content-between'} sign-in top-bar align-items-center flex-wrap flex-md-nowrap`}>
                     {
-                        active !== "biodata" && <div className="" className="back-img">
+                        active === "business" && <div className="" className="back-img">
                             <img src={BackButtonImg} alt="" onClick={handlePage}/>
                         </div>
                     }
